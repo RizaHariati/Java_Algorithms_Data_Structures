37ee4439bb0c5ee638fdc9b6f9d4668d2ee5f5c4
RizaHariati <rizahariati@yahoo.com>
2021-06-19 16:45:00 +0700

    exercise from w3resource and Awais Mirza

---
+++ src/exercise02/A4_Sort_Two_Arrays.java
@@ -0,0 +1,38 @@
+package exercise02;
+
+import java.util.Arrays;
+
+public class ScratchPad {
+
+	public static void main(String[] args) {
+		int arr1[] = {1, 2, 3, 4, 9};
+		int arr2[] = { 2, 5, 7, 8 };
+
+		int newArr[] = new int[arr1.length + arr2.length];
+		
+		int i = 0;
+		int j = 0;
+		int index = 0;
+		
+		
+		while (i < arr1.length && j < arr2.length) {
+			if (arr1[i] <= arr2[j]) {
+				newArr[index++] = arr1[i++];
+				
+			} else {
+				newArr[index++] = arr2[j++];
+			}
+		}
+		if (i == arr1.length) {
+			while (j < arr2.length) {
+				newArr[index++] = arr2[j++];
+			}
+		} else {
+			while (i < arr1.length) {
+				newArr[index++] = arr1[i++];
+			}
+		}
+		System.out.println(Arrays.toString(newArr));
+	}
+
+}
